1f0da27376c9970a0239599decb0b7433b0566f8
Regis-1 <jdudzinski101@gmail.com>
2023-06-08 19:23:09 +0200

    Day 1 completed

---
+++ day1/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <numeric>
+#include <algorithm>
+
+int main(int argc, char** argv) {
+    if (argc != 2) {
+        std::cout << "Usage: ./prog_1 <filename>" << std::endl;
+        return 1;
+    }
+
+    std::ifstream fh(argv[1]);
+
+    if (!fh) {
+        std::cout << "Invald file given." << std::endl;
+        return 1;
+    }
+
+    std::string line;
+    std::vector<int> calories;
+    std::vector<int> elves_sum;
+
+    while (std::getline(fh, line)) {
+        if (line != "")
+            calories.push_back(atoi(line.c_str()));
+        else {
+            elves_sum.push_back(std::accumulate(calories.begin(), calories.end(), 0));
+            calories.clear();
+        }
+    }
+    elves_sum.push_back(std::accumulate(calories.begin(), calories.end(), 0));
+    std::sort(elves_sum.begin(), elves_sum.end());
+
+    for (int e : elves_sum)
+        std::cout << e << std::endl;
+
+    fh.close();
+
+    return 0;
+}
